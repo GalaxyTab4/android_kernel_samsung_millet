b6a96a597ade972e79460eb0b7b2e16acb5baec9
sub77-bkp <sub77@ymail.com>
2015-06-04 03:52:53 +0200

    matissewifi: Fix Build Error if DVFS is disabled
    Change-Id: Ibefe7a769e3a5455cf37bf8fb0284ca5ef1fc63e

--- drivers/input/touchscreen/mxt1188s/mxts_1188s.c
+++ drivers/input/touchscreen/mxt1188s/mxts_1188s.c
@@ -997,8 +997,10 @@ static void mxt_report_input_data(struct mxt_data *data)
 			input_sync(data->input_dev);
 	}
 
+#ifdef CONFIG_SEC_DVFS
 #if TSP_BOOSTER
 	mxt_set_dvfs_lock(data, count);
+#endif
 #endif
 
 	data->finger_mask = 0;
@@ -2260,10 +2262,11 @@ static int mxt_stop(struct mxt_data *data)
 #if ENABLE_TOUCH_KEY
 	mxt_release_all_keys(data);
 #endif
+#ifdef CONFIG_SEC_DVFS
 #if TSP_BOOSTER
 	mxt_set_dvfs_lock(data, -1);
 #endif
-
+#endif
 	data->mxt_enabled = false;
 
 	return 0;
@@ -2413,11 +2416,13 @@ static int mxt_touch_finish_init(struct mxt_data *data)
 		goto err_req_irq;
 	}
 
+#ifdef CONFIG_SEC_DVFS
 #if TSP_BOOSTER
 	mxt_init_dvfs(data);
 #endif
 #if MXT_TKEY_BOOSTER
 	mxt_tkey_init_dvfs(data);
+#endif
 #endif
 
 	dev_info(&client->dev,  "Mxt touch controller initialized\n");
--- drivers/input/touchscreen/mxt1188s/mxts_sec_1188s.c
+++ drivers/input/touchscreen/mxt1188s/mxts_sec_1188s.c
@@ -1554,6 +1554,7 @@ struct tsp_cmd {
 	void			(*cmd_func)(void *device_data);
 };
 
+#ifdef CONFIG_SEC_DVFS
 #if TSP_BOOSTER
 static void boost_level(void *device_data)
 {
@@ -1599,6 +1600,7 @@ static void boost_level(void *device_data)
 	return;
 }
 #endif
+#endif
 
 static struct tsp_cmd tsp_cmds[] = {
 	{TSP_CMD("fw_update", fw_update),},
@@ -1629,8 +1631,10 @@ static struct tsp_cmd tsp_cmds[] = {
 	{TSP_CMD("set_tk_threshold", set_tk_threshold),},
 #endif
 #endif
+#ifdef CONFIG_SEC_DVFS
 #if TSP_BOOSTER
 	{TSP_CMD("boost_level", boost_level),},
+#endif
 #endif
 #if TSP_PATCH
 	{TSP_CMD("patch_update", patch_update),},
@@ -2038,6 +2042,7 @@ static ssize_t touchkey_report_dummy_key_store(struct device *dev,
 	return size;
 }
 
+#ifdef CONFIG_SEC_DVFS
 #if MXT_TKEY_BOOSTER
 static ssize_t boost_level_store(struct device *dev,
 				   struct device_attribute *attr,
@@ -2082,6 +2087,7 @@ static ssize_t boost_level_store(struct device *dev,
 	return count;
 }
 #endif
+#endif
 
 static DEVICE_ATTR(touchkey_d_menu, S_IRUGO | S_IWUSR | S_IWGRP, touchkey_d_menu_show, NULL);
 static DEVICE_ATTR(touchkey_d_back, S_IRUGO | S_IWUSR | S_IWGRP, touchkey_d_back_show, NULL);
@@ -2091,8 +2097,10 @@ static DEVICE_ATTR(touchkey_threshold, S_IRUGO | S_IWUSR | S_IWGRP, get_touchkey
 static DEVICE_ATTR(brightness, S_IRUGO | S_IWUSR | S_IWGRP, NULL, touchkey_led_control);
 static DEVICE_ATTR(extra_button_event, S_IRUGO | S_IWUSR | S_IWGRP,
 					touchkey_report_dummy_key_show, touchkey_report_dummy_key_store);
+#ifdef CONFIG_SEC_DVFS
 #if MXT_TKEY_BOOSTER
 static DEVICE_ATTR(boost_level, S_IWUSR | S_IWGRP, NULL, boost_level_store);
+#endif
 #endif
 
 static struct attribute *touchkey_attributes[] = {
@@ -2103,8 +2111,10 @@ static struct attribute *touchkey_attributes[] = {
 	&dev_attr_touchkey_threshold.attr,
 	&dev_attr_brightness.attr,
 	&dev_attr_extra_button_event.attr,
+#ifdef CONFIG_SEC_DVFS
 #if MXT_TKEY_BOOSTER
 	&dev_attr_boost_level.attr,
+#endif
 #endif
 	NULL,
 };
@@ -2736,6 +2746,7 @@ static void  mxt_sysfs_remove(struct mxt_data *data)
 #endif
 }
 
+#ifdef CONFIG_SEC_DVFS
 #if TSP_BOOSTER
 static void mxt_change_dvfs_lock(struct work_struct *work)
 {
@@ -2860,6 +2871,8 @@ void mxt_init_dvfs(struct mxt_data *data)
 	data->dvfs_lock_status = false;
 }
 #endif
+#endif
+#ifdef CONFIG_SEC_DVFS
 #if MXT_TKEY_BOOSTER
 static void mxt_tkey_change_dvfs_lock(struct work_struct *work)
 {
@@ -2948,4 +2961,5 @@ void mxt_tkey_init_dvfs(struct mxt_data *data)
 
 	data->tkey_dvfs_lock_status = true;
 }
+#endif
 #endif
